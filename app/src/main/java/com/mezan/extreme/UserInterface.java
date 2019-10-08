4e68b245a3745c23e8fe6e491f26d0726ed07af8
mezan <mrmezan0@gmail.com>
2019-10-09 05:50:55 +0600

    Location finder added and business card - UI designed

--- app/src/main/java/com/mezan/extreme/UserInterface.java
+++ app/src/main/java/com/mezan/extreme/UserInterface.java
@@ -1,24 +1,164 @@
 package com.mezan.extreme;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 
 public class UserInterface extends AppCompatActivity {
 
 
+    public  static final int RequestPermissionCode  = 1 ;
+
+    Context context;
+    Intent intent1 ;
+    LocationManager locationManager ;
+    boolean GpsStatus = false ;
+    Criteria criteria ;
+    String Holder;
+    FusedLocationProviderClient fusedLocationClient;
+
+    CardView bikeCard,carCard,foodCard,parcelCard;
+    LinearLayout root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_interface);
 
+        EnableRuntimePermission();
+
+       //Card
+        bikeCard = findViewById(R.id.bikeCard);
+        carCard = findViewById(R.id.carCard);
+        foodCard = findViewById(R.id.foodCard);
+        parcelCard = findViewById(R.id.parcelCard);
+        root = findViewById(R.id.rootUserInterface);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        criteria = new Criteria();
+
+        Holder = locationManager.getBestProvider(criteria, false);
+
+        context = getApplicationContext();
+
+        CheckGpsStatus();
+
+        //Card OnClick Listener
+        bikeCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isInternetConnection())
+                gettingLocationLatLon();
+                else
+                    Snackbar.make(root,"No Internet Available",Snackbar.LENGTH_LONG).show();
+
+            }
+        });
+        carCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isInternetConnection())
+                    gettingLocationLatLon();
+                else
+                    Snackbar.make(root,"No Internet Available",Snackbar.LENGTH_LONG).show();
+
+            }
+        });
+        foodCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isInternetConnection())
+                    gettingLocationLatLon();
+                else
+                    Snackbar.make(root,"No Internet Available",Snackbar.LENGTH_LONG).show();
+
+            }
+        });
+        parcelCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isInternetConnection())
+                    gettingLocationLatLon();
+                else
+                    Snackbar.make(root,"No Internet Available",Snackbar.LENGTH_LONG).show();
+
+            }
+        });
 
 
+    }
 
+    private void gettingLocationLatLon(){
+        CheckGpsStatus();
+
+        if(GpsStatus) {
+            if (Holder != null) {
+                if (ActivityCompat.checkSelfPermission(
+                        UserInterface.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        &&
+                        ActivityCompat.checkSelfPermission(UserInterface.this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+
+                /*This line is main work in location and must include gradle of google play services and also coarse location and internet connection must be enable*/
+
+                fusedLocationClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(UserInterface.this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                if(location != null){
+                                    Log.d("LocationExtreme", String.valueOf(location.getLongitude()));
+                                    Log.d("LocationExtreme",String.valueOf(location.getLatitude()));
+                                    Snackbar.make(root,"(Lat,Lon)=("+location.getLatitude()+","+location.getLongitude()+")",Snackbar.LENGTH_LONG).show();
+                                }else {
+                                    Log.d("LocationExtreme", "No Location");
+                                    Snackbar.make(root,"Location not found!",Snackbar.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+                /*Location(Latitude,Longitude)*/
+
+
+            }
+        }else {
+
+            Snackbar.make(root,"GPS Enable First.Then Try Again!",Snackbar.LENGTH_LONG).show();
+
+        }
+    }
+
+    private void settingGPS(){
+        intent1 = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent1);
     }
 
 
@@ -30,20 +170,17 @@ public class UserInterface extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+
         if (id == R.id.menuSignOut) {
 
             /*FirebaseAuth.getInstance().signOut();
             Intent intent=new Intent(getApplicationContext(), LoginActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
-            finish();
-*/
+            finish();*/
+
 
             //user don't want to use again again verification code for those sign out
             //so i am just back him in login page
@@ -59,7 +196,71 @@ public class UserInterface extends AppCompatActivity {
             startActivity(intent);
             return true;
         }
+        if (id == R.id.gpsSetting){
+            settingGPS();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void CheckGpsStatus(){
+
+        locationManager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+
+        GpsStatus = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+    }
+
+    public void EnableRuntimePermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(UserInterface.this,
+                Manifest.permission.ACCESS_FINE_LOCATION))
+        {
+            Snackbar.make(root,"GPS Permission must be allowed us!",Snackbar.LENGTH_LONG).show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(UserInterface.this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION}, RequestPermissionCode);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int RC, String per[], int[] PResult) {
+
+        switch (RC) {
+
+            case RequestPermissionCode:
+
+                if (PResult.length > 0 && PResult[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Snackbar.make(root,"GPS Permission Granted!",Snackbar.LENGTH_LONG).show();
+
+                } else {
+
+                    Snackbar.make(root,"GPS Permission Cancelled!",Snackbar.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
+    public boolean isInternetConnection() {
+
+        ConnectivityManager conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        // ARE WE CONNECTED TO THE NET
+        if (conMgr.getActiveNetworkInfo() != null
+                && conMgr.getActiveNetworkInfo().isAvailable()
+                && conMgr.getActiveNetworkInfo().isConnected()) {
+
+            return true;
+
+        } else {
+            return false;
+        }
+
+
+    }
+
 }
