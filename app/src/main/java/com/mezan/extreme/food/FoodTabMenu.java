bd724315277944e1b0ea10192d7b9ae4db5492aa
mezan <mrmezan0@gmail.com>
2019-12-01 01:19:14 +0600

    Food Panda Layout copy complete

--- app/src/main/java/com/mezan/extreme/food/FoodTabMenu.java
+++ app/src/main/java/com/mezan/extreme/food/FoodTabMenu.java
@@ -3,9 +3,13 @@ package com.mezan.extreme.food;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 import com.mezan.extreme.R;
@@ -14,16 +18,52 @@ public class FoodTabMenu extends AppCompatActivity {
 
     TabLayout tabLayout;
     ViewPager viewPager;
+
+    int []image = {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,
+            R.drawable.f,R.drawable.g,R.drawable.h,R.drawable.i,R.drawable.j,
+            R.drawable.k,R.drawable.l,R.drawable.m,R.drawable.n,R.drawable.o,
+            R.drawable.p,R.drawable.q,R.drawable.r,R.drawable.s,R.drawable.t};
+    float []Rating = {3.5f,4f,5f,4.5f,5f,
+            4f,4.5f,5f,4.5f,5f,
+            3.5f,4f,5f,4.5f,5f,
+            4f,4.5f,5f,4.5f,5f
+    };
+    String name[] = new String[25];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //Full Screen Feature
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_food_tab_menu);
+
+        int positionHotel=0;
+        Intent it = getIntent();
+        Bundle bd = it.getExtras();
+        if (bd != null){
+            positionHotel = bd.getInt("index",0);
+        }
+
+        name = getResources().getStringArray(R.array.hotelName);
+
+        TextView nameTxt,deliveryTimeTxt;
+        RatingBar hotelRating;
+        nameTxt = findViewById(R.id.foodHotelName);
+        deliveryTimeTxt = findViewById(R.id.foodHotelDeliveryTime);
+        hotelRating = findViewById(R.id.foodHotelRating);
+        LinearLayout hotelImage;
+        hotelImage = findViewById(R.id.foodHotelImage);
+
+        //hotel identification
+        nameTxt.setText(name[positionHotel]);
+        deliveryTimeTxt.setText("45\nMIN");
+        hotelRating.setRating(Rating[positionHotel]);
+        hotelImage.setBackgroundResource(image[positionHotel]);
+
 
         tabLayout= findViewById(R.id.tabLayout);
         viewPager= findViewById(R.id.viewPager);
