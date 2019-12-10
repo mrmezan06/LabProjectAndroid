d941a5215e694343ca7fad5bd6c32e17e87dc45a
mezan <mrmezan0@gmail.com>
2019-12-10 21:02:23 +0600

    All Tab updated

--- app/src/main/java/com/mezan/extreme/food/SetMenu.java
+++ app/src/main/java/com/mezan/extreme/food/SetMenu.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -50,42 +51,26 @@ public class SetMenu extends Fragment {
         appetizerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
-                final DatabaseReference addCartDB = FirebaseDatabase.getInstance().getReference().child("addcart").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
-                /*addCartDB.child("food").setValue(name[i]);
-                addCartDB.child("price").setValue(price[i]);
-                addCartDB.child("quantity").setValue("1");*/
 
-                addCartDB.addListenerForSingleValueEvent(new ValueEventListener() {
+
+                //already pending order has or not
+                final DatabaseReference orderDB = FirebaseDatabase.getInstance().getReference().child("Order").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                orderDB.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if (dataSnapshot.exists()) {
-                            if (dataSnapshot.child(name[i]).exists()) {
+                        if (dataSnapshot.exists()){
 
-                                String qtyStr = dataSnapshot.child(name[i]).child("Quantity").getValue().toString();
-                                int qty = Integer.parseInt(qtyStr) + 1;
-
-                                String priceStr = dataSnapshot.child(name[i]).child("Price").getValue().toString();
-                                double price = Double.parseDouble(priceStr);
-
-                                double tprice = price * qty;
-
-                                dataSnapshot.child(name[i]).child("totalPrice").getRef().setValue(String.valueOf(tprice));
-                                dataSnapshot.child(name[i]).child("Quantity").getRef().setValue(String.valueOf(qty));
-                            } else {
-                                dataSnapshot.getRef().child(name[i]).child("Name").setValue(name[i]);
-                                dataSnapshot.getRef().child(name[i]).child("Name").setValue(name[i]);
-                                dataSnapshot.getRef().child(name[i]).child("Price").setValue(price[i]);
-                                dataSnapshot.getRef().child(name[i]).child("totalPrice").setValue(price[i]);
-                                dataSnapshot.getRef().child(name[i]).child("Quantity").setValue("1");
+                            if (dataSnapshot.child("status").exists()){
+                                String status = dataSnapshot.child("status").getValue().toString();
+                                if (status.equals("pending")){
+                                    Toast.makeText(getContext(),"Already have a pending order",Toast.LENGTH_LONG).show();
+                                }else {
+                                    addtocart(i);
+                                }
                             }
 
-                        } else {
-                            DatabaseReference newUserAddCartDB = FirebaseDatabase.getInstance().getReference().child("addcart").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
-                            newUserAddCartDB.child(name[i]).child("Name").setValue(name[i]);
-                            newUserAddCartDB.child(name[i]).child("Name").setValue(name[i]);
-                            newUserAddCartDB.child(name[i]).child("Price").setValue(price[i]);
-                            newUserAddCartDB.child(name[i]).child("totalPrice").setValue(price[i]);
-                            newUserAddCartDB.child(name[i]).child("Quantity").setValue("1");
+                        }else {
+                            addtocart(i);
                         }
                     }
 
@@ -96,8 +81,68 @@ public class SetMenu extends Fragment {
                 });
 
 
+
+
+
+
+
             }});
 
         return view;
+    }
+    public void addtocart(final int i){
+        final DatabaseReference addCartDB = FirebaseDatabase.getInstance().getReference().child("addcart").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                /*addCartDB.child("food").setValue(name[i]);
+                addCartDB.child("price").setValue(price[i]);
+                addCartDB.child("quantity").setValue("1");*/
+
+        addCartDB.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                try {
+                    if (dataSnapshot.exists()) {
+
+                        if (dataSnapshot.child(name[i]).exists()){
+                            String qtyStr = dataSnapshot.child(name[i]).child("Quantity").getValue().toString();
+                            int qty = Integer.parseInt(qtyStr) + 1;
+
+                            String priceStr = dataSnapshot.child(name[i]).child("Price").getValue().toString();
+                            double price = Double.parseDouble(priceStr);
+
+                            double tprice = price * qty;
+
+                            dataSnapshot.child(name[i]).child("totalPrice").getRef().setValue(String.valueOf(tprice));
+                            dataSnapshot.child(name[i]).child("Quantity").getRef().setValue(String.valueOf(qty));
+                        }else {
+                            dataSnapshot.getRef().child(name[i]).child("Name").setValue(name[i]);
+                            dataSnapshot.getRef().child(name[i]).child("Name").setValue(name[i]);
+                            dataSnapshot.getRef().child(name[i]).child("Price").setValue(price[i]);
+                            dataSnapshot.getRef().child(name[i]).child("totalPrice").setValue(price[i]);
+                            dataSnapshot.getRef().child(name[i]).child("Quantity").setValue("1");
+                        }
+                    }else {
+                        try {
+                            DatabaseReference newUserAddCartDB = FirebaseDatabase.getInstance().getReference().child("addcart").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                            newUserAddCartDB.child(name[i]).child("Name").setValue(name[i]);
+                            newUserAddCartDB.child(name[i]).child("Name").setValue(name[i]);
+                            newUserAddCartDB.child(name[i]).child("Price").setValue(price[i]);
+                            newUserAddCartDB.child(name[i]).child("totalPrice").setValue(price[i]);
+                            newUserAddCartDB.child(name[i]).child("Quantity").setValue("1");
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        Toast.makeText(getContext(),"Item added to the cart",Toast.LENGTH_SHORT).show();
     }
 }
